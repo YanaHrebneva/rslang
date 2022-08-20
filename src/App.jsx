d07855223123e3fea36ea242a73f032a505cbcca
Anastasiya <nastagh96@mail.ru>
2022-08-20 19:44:02 +0200

    feat: add eslint fonts scss

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import { Route, Routes } from 'react-router-dom'
-import './App.css'
-import Layout from './components/Layout'
-import HomePage from './pages/HomePage'
-import BookPage from './pages/BookPage'
-import MinigamePage from './pages/MinigamePage'
-import StatisticPage from './pages/StatisticPage'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import './App.scss';
+import Layout from './components/Layout';
+import HomePage from './pages/HomePage';
+import BookPage from './pages/BookPage';
+import MinigamePage from './pages/MinigamePage';
+import StatisticPage from './pages/StatisticPage';
+import Error from './pages/Error';
 
 function App() {
-	return (
-		<>
-			<Routes>
-				<Route path='/' element={<Layout />}>
-					<Route index element={<HomePage />} />
-					<Route path='book' element={<BookPage />} />
-					<Route path='minigame' element={<MinigamePage />} />
-					<Route path='statistic' element={<StatisticPage />} />
-					<Route path='*' element={<HomePage />} />
-				</Route>
-			</Routes>
-		</>
-	)
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index path="home" element={<HomePage />} />
+        <Route path="book" element={<BookPage />} />
+        <Route path="minigame" element={<MinigamePage />} />
+        <Route path="statistic" element={<StatisticPage />} />
+        <Route path="*" element={<Error />} />
+      </Route>
+    </Routes>
+  );
 }
-export default App
+export default App;
