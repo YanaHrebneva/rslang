91a94de6ef371bdd77f64f3a092acf652549d01f
Андрей <andrej@MacBook-Air.local>
2022-08-23 23:22:32 +0300

    feat:receiving data from the server

--- src/pages/Book/BookPage.jsx
+++ src/pages/Book/BookPage.jsx
@@ -1,7 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 
 export default function BookPage() {
+  const [words, setWords] = useState();
+  const [page, setPage] = useState(1);
+  const [groups, setGroups] = useState(1);
+  const [pageQty, setPageQty] = useState(0);
+
+  useEffect(() => {
+    const baseUrl = 'https://rslang-yanahrebneva.herokuapp.com/words?';
+    axios.get(`${baseUrl}group=${groups - 1}&page=${page - 1}`).then(({ data }) => {
+      const allWords = data;
+      console.log(data);
+      setWords(allWords);
+      const pageQtyLength = allWords.length;
+      setPageQty(pageQtyLength);
+    });
+  }, [page, groups]);
+
   return (
-    <div>book</div>
+    <div>{console.log(words)}</div>
   );
 }
