ad4d67d8bfb17d16eb2e6805a326fd61eb290181
Андрей <andrej@MacBook-Air.local>
2022-09-04 13:05:37 +0300

    ferat:added user learning progress

--- src/pages/Book/CardBook.jsx
+++ src/pages/Book/CardBook.jsx
@@ -3,7 +3,7 @@ import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
-import { CardActionArea, CardActions } from '@mui/material';
+import { Box, CardActionArea, CardActions } from '@mui/material';
 import { BASE_URL } from '../../constants/url';
 import ButtonsActions from './ButtonsActions';
 
@@ -66,9 +66,18 @@ export default function CardBook(props) {
           <Typography gutterBottom variant="subtitle4" component="div">
             {textExampleTranslate}
           </Typography>
-          <Typography variant="subtitle2" color="text.secondary" />
         </CardContent>
       </CardActionArea>
+      <Box style={userId ? { visibility: 'visible' } : { visibility: 'hidden' }}>
+        <Typography gutterBottom variant="subtitle2" component="div">
+          использовано:0
+        </Typography>
+        <Typography gutterBottom variant="subtitle2" component="div">
+          угадано:0
+        </Typography>
+
+      </Box>
+
       <CardActions sx={{ display: 'flex', justifyContent: 'center' }}>
         <ButtonsActions
           toggleState={toggleState}
